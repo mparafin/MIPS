6f9f17f5838e23ee58f3f98ad3d4f2d2af73a377
Michał Parafiniuk <michal.a.parafiniuk@gmail.com>
2018-03-20 21:23:29 +0100

    Basic commit

---
+++ PROJEKT_MIPS.asm
@@ -0,0 +1,6 @@
+.data
+intro:	.ascii	"The Huffman Coding - MIPS \"Big Project\"\n"
+	.asciiz "Copyright: Politechnika Warszawska, Author: Michal Parafiniuk, no. 285634\n"
+prompt1:	.asciiz	"Please input the path to the file with the text you want to encode/decode: "
+prompt2:	.asciiz	"Do you want to decode? (leave empty if not)"
+
