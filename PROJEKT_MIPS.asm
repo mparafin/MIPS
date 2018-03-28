d0aa5b53df6a8448127d3710c3b7a4421509bf35
Windows domowy <michciu1@gmail.com>
2018-03-28 21:44:36 +0200

    28.03.2018 Working statistics and buffer juggling

--- PROJEKT_MIPS.asm
+++ PROJEKT_MIPS.asm
@@ -48,26 +48,34 @@ syscall #wypluj error
 j getInput
 
 preLoad:
-la $s0, ascii_stats
-la $t9, input_buffer
+add $s7, $0, $v0 # zapisz identyfikator otwartego pliku w $s0
 Load:
-add $t0, $0, $v0
 li $v0, 14
-add $a0, $0, $t0
+add $a0, $0, $s7
 la $a1, input_buffer
 li $a2, 1000
 syscall #wczytaj plik do bufora (a przynajmniej pierwsze lub kolejne 1000 bajtów)
 
 
 #STATYSTYKA ZNAKÓW TEKSTU
+beqz $v0, stat_end
+add $t8, $0, $v0	# przygotowanie licznika znaków w buforze (zapezpieczenie przed wyjœciem poza bufor lub za³adowane znaki)
+la $t9, input_buffer	# przygotowanie adresu pocz¹tku pobranej porcji wejœciowego pliku
+la $s0, ascii_stats	# przygotowanie adresu tablicy zliczaj¹cej wyst¹pienia znaków
+
 stat_loop:
 lb $t0, ($t9)		# w $t0 mamy bie¿¹cy znak
+beq $t0, $0, stat_end	# je¿eli napotkamy \0, koñczymy procedurê
 addu $t1, $s0, $t0	# tworzymy adres do ascii_stats z przesuniêciem równym wartoœci ascii
 lb $t2, ($t1)		# ³adujemy do $t2 obecn¹ wartoœæ zliczonych znaków tego rodzaju
 addi $t2, $t2, 1	# inkrementujemy
 sb $t2, ($t1)		# odstawiamy na miejsce
 addiu $t9, $t9, 1	# przesuwamy siê o znak do przodu
+addi $t8, $t8, -1	# zmniejszamy licznik
+bnez $t8, stat_loop	# je¿eli nie wyszliœmy poza zakres, powtarzamy
+beqz $t8, Load		# je¿eli skoñczy³ siê bufor, za³aduj nowy fragment pliku
 
+stat_end:
 
 li $v0, 4
 la $a0, input_buffer
@@ -78,10 +86,10 @@ syscall #zabij siê
 
 
 pathSanitization: #usuniêcie \n z koñca œcie¿ki
-li $s0, '\n'
+li $t8, '\n'
 PS_loop:
 lb $t1, ($a0)
-beq $t1, $s0, destroy
+beq $t1, $t8, destroy
 addiu $a0, $a0, 1
 j PS_loop
 destroy:
